439d575aa2251972bb82fb4e777a3c205f99baf0
Isaiah258 <hideonbush0061@gmail.com>
2025-11-27 00:51:06 +0800

    fix exp bugs

--- app/src/main/java/com/gosnow/app/ui/login/TermsScreen.kt
+++ app/src/main/java/com/gosnow/app/ui/login/TermsScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -21,6 +22,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TermsScreen(onBackClick: () -> Unit) {
     val scrollState = rememberScrollState()
